e9480830b99cce5196304948a52fec8bab684d72
GiannimarasDim <giannimarasdi@gmail.com>
2022-09-27 18:34:08 +0300

    Initial Commit

---
+++ src/gr/aueb/sev/vars/IntsApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.sev.vars;
+
+public class IntsApp {
+
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=10;
+        int num3=10;
+        num3=num1+num2;
+        System.out.println(num3);
+    }
+}
