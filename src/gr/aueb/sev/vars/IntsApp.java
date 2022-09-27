cd1cf2fccd5e9f9ed73f322fab6b2cc4e26369d9
GiannimarasDim <giannimarasdi@gmail.com>
2022-09-27 19:09:55 +0300

    ints v2.0

--- src/gr/aueb/sev/vars/IntsApp.java
+++ src/gr/aueb/sev/vars/IntsApp.java
@@ -3,7 +3,7 @@ package gr.aueb.sev.vars;
 public class IntsApp {
 
     public static void main(String[] args) {
-        int num1=10;
+        int num1=30;
         int num2=10;
         int num3=10;
         num3=num1+num2;
